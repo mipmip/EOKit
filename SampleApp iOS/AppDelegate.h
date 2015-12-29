859378c85ef3f60ebc364df3f2c43a7713fb2ff7
Pim Snel <pim@lingewoud.nl>
2015-12-29 12:52:59 +0100

    Add ios sample app

---
+++ SampleApp iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleApp iOS
+//
+//  Created by Pim Snel on 29-12-15.
+//  Copyright © 2015 Lingewoud BV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleApp iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleApp iOS
+//
+//  Created by Pim Snel on 29-12-15.
+//  Copyright © 2015 Lingewoud BV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
